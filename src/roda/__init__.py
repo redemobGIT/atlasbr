d753bbbedee71e7182e988d51210ba8b7fc25c41
Hugo Oliveira <hg.oliveira2ds@gmail.com>
2025-10-02 17:13:01 -0300

    add: create __init__.py for roda package initialization

---
+++ src/roda/__init__.py
@@ -0,0 +1,15 @@
+"""Pacote principal da biblioteca RODA.
+
+Este arquivo marca o diretório `roda` como um pacote Python e expõe
+alguns submódulos de nível superior para conveniência.
+"""
+
+# Reexporta namespaces úteis (importação tardia para evitar custos na importação)
+__all__ = [
+    "fetch_data",
+    "processing",
+    "utils",
+]
+
+
+
